595a988cffa29687dab2a4919ae4b479a3c8b41f
Mochamad Gufron <mgufronefendi@gmail.com>
2016-07-02 08:48:02 +0700

    improve more testing

--- tests/WhmcsTest.php
+++ tests/WhmcsTest.php
@@ -23,6 +23,7 @@ class WhmcsTest extends PHPUnit_Framework_TestCase
       json_encode(['result'=>'error','message'=>'Invalid IP : 127.0.0.1']),
       json_encode(['result'=>'error','message'=>'commant not found']),
       json_encode(['result'=>'success','clients'=>['client'=>[]]]),
+      json_encode(['result'=>'success','clients'=>['client'=>[]]]),
       json_encode(['result'=>'success','clients'=>['client'=>[]]])
     ];
     $mock = new MockHandler([
@@ -30,6 +31,7 @@ class WhmcsTest extends PHPUnit_Framework_TestCase
       new Response(403, ["Content-Type"=>"text/json"], $content[1]),
       new Response(403, ["Content-Type"=>"text/json"], $content[2]),
       new Response(200, ["Content-Type"=>"text/json"], $content[3]),
+      new Response(200, ["Content-Type"=>"text/json"], $content[4]),
       new Response(200, ["Content-Type"=>"text/json"], $content[4]),
       new Response(200, ["Content-Type"=>"text/json"], $content[4]),
     ]);
@@ -88,18 +90,17 @@ class WhmcsTest extends PHPUnit_Framework_TestCase
       'password'=>'jago4n123',
       'authType'=>'keys'
     ]);
-    $whmcs =  new Whmcs($this->config);
-    // $whmcs = $this->whmcs;
-    $responses = json_encode(['result'=>'success','clients'=>['client'=>[]]]);
-
-    $mock = new MockHandler([
-      new Response(200, ["Content-Type"=>"text/json"], $responses),
-    ]);
-    $handler = HandlerStack::create($mock);
-    $whmcs->client([
-      'handler'=>$handler,
-    ]);
+    $whmcs = new Whmcs($this->config);
     $response = $whmcs->getclients();
+    $request = $whmcs->getRequest();
+    $data = http_build_query([
+      'action'=>'getclients',
+      'username'=>'gufron',
+      'accesskey'=>'jago4n123',
+      'responsetype'=>'json'
+    ]);
+    $this->assertEquals('http://localhost/includes/api.php', $request->getUri());
+    $this->assertEquals($data, $request->getBody()->getContents());
     // print_r($whmcs->getRequest());
     // $this->assertTrue(isset($response['clients']));
   }
